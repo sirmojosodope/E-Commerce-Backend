c9d2a3253a970d9ad458b440d8f0403efee5fbef
sirmojosodope <stoverlee29@gmail.com>
2022-05-12 18:19:49 -0400

    Updating Models

---
+++ models/Tag.js
@@ -0,0 +1,14 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+class Tag extends Model {}
+
+Tag.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+});
